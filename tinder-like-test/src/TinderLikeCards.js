f8bf60798f8d38d3039825fd6339d578d05b3b62
Ahmed <amiiri.ahmed@gmail.com>
2022-01-31 20:28:58 +0100

    ...

--- tinder-like-test/src/TinderLikeCards.js
+++ tinder-like-test/src/TinderLikeCards.js
@@ -1,27 +1,13 @@
 import React, {useState} from "react";
 import TinderCard from "react-tinder-card";
 import './TinderLikeCards.css';
+import Data from './data/data.json';
 
 function TinderLikeCards() {
-    //init variables for cards
-    const [people, setPeople] = useState([
-        {
-            albumId: 1,
-            id: 1,
-            title: "accusamus beatae ad facilis cum similique qui sunt",
-            url: "https://via.placeholder.com/600/92c952"
-        },
-        {
-            albumId: 1,
-            id: 2,
-            title: "reprehenderit est deserunt velit ipsam",
-            url: "https://via.placeholder.com/600/771796"
-        }
-    ]);
     return (
         <div>
             <div className="container">
-                {people.map(person => (
+                {Data.map(person=> (
                     <TinderCard className="swipeCard" key={person.id} preventSwipe={['up','down']}>
                         <div style={{backgroundImage: `url(${person.url})`}} className="card">
                             <h3>{person.title}</h3>
