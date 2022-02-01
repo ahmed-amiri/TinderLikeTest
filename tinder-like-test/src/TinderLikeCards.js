711ed1982761406476363a21654754ae0fe2c8bd
Ahmed <amiiri.ahmed@gmail.com>
2022-02-01 20:52:44 +0100

    liked/disliked

--- tinder-like-test/src/TinderLikeCards.js
+++ tinder-like-test/src/TinderLikeCards.js
@@ -3,12 +3,21 @@ import TinderCard from "react-tinder-card";
 import './TinderLikeCards.css';
 import Data from './data/data.json';
 
+const onSwipe = (direction) => {
+    if (direction == "right") {
+        console.log("liked")
+    }
+    else if(direction=="left") {
+        console.log("disliked")
+    }
+}
+
 function TinderLikeCards() {
     return (
         <div>
             <div className="container">
                 {Data.map(person=> (
-                    <TinderCard className="swipeCard" key={person.id} preventSwipe={['up','down']}>
+                    <TinderCard className="swipeCard" key={person.id} preventSwipe={['up','down']} onSwipe={onSwipe}>
                         <div style={{backgroundImage: `url(${person.url})`}} className="card">
                             <h3>{person.title}</h3>
                         </div>
